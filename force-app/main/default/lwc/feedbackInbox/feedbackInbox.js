60adc3a5de8c898bb0c272a08b764a430f4b1b97
Ece Yildiz <yildiz.e@northeastern.edu>
2025-07-27 18:51:27 -0700

    changing feedback inbox logic and design

--- force-app/main/default/lwc/feedbackInbox/feedbackInbox.js
+++ force-app/main/default/lwc/feedbackInbox/feedbackInbox.js
@@ -1,28 +1,32 @@
 import { LightningElement, api, wire, track } from 'lwc';
-import getResponses from '@salesforce/apex/FeedbackResponseController.getResponses';
+import getTicketHistory from '@salesforce/apex/FeedbackResponseController.getTicketHistory';
 import { subscribe, unsubscribe, MessageContext, publish } from 'lightning/messageService';
 import FEEDBACK_CHANNEL from '@salesforce/messageChannel/FeedbackChannel__c';
 import { refreshApex } from '@salesforce/apex';
 
 export default class FeedbackInbox extends LightningElement {
-    @api recordId;
-    @track responses = [];
+    @track recordId = null;
+    @track ticketHistory = null;
+    @track historyEntries = [];
     @track error;
-    wiredResponsesResult;
+    wiredHistoryResult;
     subscription = null;
 
     @wire(MessageContext)
     messageContext;
 
-    @wire(getResponses, { feedbackId: '$recordId' })
-    wiredResponses(result) {
-        this.wiredResponsesResult = result;
-        if (result.data) {
-            this.responses = result.data;
+    @wire(getTicketHistory, { feedbackId: '$recordId' })
+    wiredHistory(result) {
+        this.wiredHistoryResult = result;
+        if (result.data && result.data !== null) {
+            this.ticketHistory = result.data;
+            this.historyEntries = result.data.historyEntries || [];
             this.error = undefined;
+            console.log('Ticket History:', this.ticketHistory);
         } else if (result.error) {
             this.error = result.error;
-            this.responses = [];
+            this.ticketHistory = null;
+            this.historyEntries = [];
         }
     }
 
@@ -53,18 +57,50 @@ export default class FeedbackInbox extends LightningElement {
 
     handleMessage(message) {
         if (message.type === 'replyAdded' && message.feedbackId === this.recordId) {
-            refreshApex(this.wiredResponsesResult);
+            refreshApex(this.wiredHistoryResult);
         } else if (message.type === 'feedbackSelected') {
-            // Update to show responses for the selected feedback
+            // Update to show history for the selected feedback
             this.recordId = message.feedbackId;
+        } else if (message.type === 'requestCurrentFeedback') {
+            // Respond with current selection
+            if (this.recordId) {
+                publish(this.messageContext, FEEDBACK_CHANNEL, {
+                    type: 'feedbackSelected',
+                    feedbackId: this.recordId
+                });
+            }
         }
     }
 
-    get hasResponses() {
-        return this.responses && this.responses.length > 0;
+    requestCurrentSelection() {
+        // Request current feedback selection from statusManager
+        publish(this.messageContext, FEEDBACK_CHANNEL, {
+            type: 'requestCurrentFeedback'
+        });
     }
 
-    get responseCount() {
-        return this.responses ? this.responses.length : 0;
+    connectedCallback() {
+        this.subscribeToMessageChannel();
+        // Request current selection when component loads
+        setTimeout(() => {
+            this.requestCurrentSelection();
+        }, 100);
+    }
+
+    get showInbox() {
+        return this.recordId != null;
+    }
+
+    get hasHistory() {
+        return this.historyEntries && this.historyEntries.length > 0;
+    }
+
+    get ticketName() {
+        return this.ticketHistory && this.ticketHistory.feedback ? 
+               this.ticketHistory.feedback.Name : this.recordId;
+    }
+
+    get selectedFeedbackName() {
+        return this.ticketName;
     }
 } 
